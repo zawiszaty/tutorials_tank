3b93bebb7fdaa915ad58db398cdaca050584969f
zawiszaty <ZawiszaCzarny52@gmail.com>
2018-10-21 12:08:53 +0200

    rabbit not work i skip this task

---
+++ src/Infrastructure/Share/Event/Consumer/SendEventsToElasticConsumer.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zawiszaty
+ * Date: 20.10.18
+ * Time: 14:02
+ */
+
+namespace App\Infrastructure\Share\Event\Consumer;
+
+
+class SendEventsToElasticConsumer
+{
+
+}
---
+++ src/Infrastructure/Share/Event/Query/EventElasticRepository.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zawiszaty
+ * Date: 20.10.18
+ * Time: 14:04
+ */
+
+namespace App\Infrastructure\Share\Event\Query;
+
+
+class EventElasticRepository
+{
+
+}
